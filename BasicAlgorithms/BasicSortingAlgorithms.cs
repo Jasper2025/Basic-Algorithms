34f051856028390696aec61522b463bfa1b65015
Pedram T'Kanchi <jasper@zocardo.com>
2016-07-20 08:02:15 -0700

    Made code prettier in Test.c and BasicSortingAlgorithms.cs

--- BasicAlgorithms/BasicSortingAlgorithms.cs
+++ BasicAlgorithms/BasicSortingAlgorithms.cs
@@ -1,6 +1,6 @@
 namespace BasicAlgorithms
 {
-    public partial class BasicSortingAlgorithms /* Contains BubbleSort, InsertionSort, SelectionSort */
+    public partial class BasicSortingAlgorithms
     {
         /* https://en.wikipedia.org/wiki/Bubble_sort */
         public static void BubbleSort(int[] randomArray)
@@ -26,11 +26,13 @@ namespace BasicAlgorithms
             {
                 int j = i;
                 int toInsert = unsortedArray[j];
+
                 while (j > 0 && unsortedArray[j - 1] > toInsert)
                 {
                     unsortedArray[j] = unsortedArray[j - 1];
                     j--;
                 }//while
+
                 unsortedArray[j] = toInsert;
             }//for
         }//InsertionSort
@@ -41,114 +43,103 @@ namespace BasicAlgorithms
             for (int i = 0; i < unsortedArray.Length; i++)
             {
                 int startIndex = i;
+
                 for (int j = i; j < unsortedArray.Length; j++)
                 {
                     if (unsortedArray[startIndex] > unsortedArray[j])
-                    {
                         startIndex = j;
-                    }//if
+
                 }//for
+
                 int temp = unsortedArray[i];
                 unsortedArray[i] = unsortedArray[startIndex];
                 unsortedArray[startIndex] = temp;
             }//for
         }//SelectionSort
-    }
+    }//Partial Class - BasicSortingAlgorithms
 
-    public partial class BasicSortingAlgorithms /* Contains MergeSort */
+    public partial class BasicSortingAlgorithms
     {
         /* https://en.wikipedia.org/wiki/Merge_sort */
-        private static void Merge(int[] left, int[] right, int[] result)
+        private static void Merge(int[] leftArray, int[] rightArray, int[] combinedArray)
         {
-            int leftIndex = 0, rightIndex = 0, resultIndex = 0;
+            int leftArrIndex = 0, rightArrIndex = 0, combinedArrIndex = 0;
 
-            while (leftIndex < left.Length && rightIndex < right.Length)
+            while (leftArrIndex < leftArray.Length && rightArrIndex < rightArray.Length)
             {
-                if (left[leftIndex] <= right[rightIndex])
-                {
-                    result[resultIndex++] = left[leftIndex++];
-                }//if
+                if (leftArray[leftArrIndex] <= rightArray[rightArrIndex])
+                    combinedArray[combinedArrIndex++] = leftArray[leftArrIndex++];
                 else
-                {
-                    result[resultIndex++] = right[rightIndex++];
-                }//else
+                    combinedArray[combinedArrIndex++] = rightArray[rightArrIndex++];
             }//while
 
-            while (leftIndex < left.Length)
-            {
-                result[resultIndex++] = left[leftIndex++];
-            }//while
+            while (leftArrIndex < leftArray.Length)
+                combinedArray[combinedArrIndex++] = leftArray[leftArrIndex++];
 
-            while (rightIndex < right.Length)
-            {
-                result[resultIndex++] = right[rightIndex++];
-            }//while
+            while (rightArrIndex < rightArray.Length)
+                combinedArray[combinedArrIndex++] = rightArray[rightArrIndex++];
         }//Merge
 
         public static void MergeSort(int[] array)
         {
             if (array.Length < 2)
-            {
                 return;
-            }//if
 
-            int mid = array.Length / 2;
-            int[] left = new int[mid];
-            int[] right = new int[array.Length - mid];
+            int midArr = array.Length / 2;
+            int[] leftArray = new int[midArr];
+            int[] rightArray = new int[array.Length - midArr];
 
-            for (int i = 0; i < mid; i++)
-            {
-                left[i] = array[i];
-            }//for
+            for (int i = 0; i < midArr; i++)
+                leftArray[i] = array[i];
 
             int index = 0;
 
-            for (int j = mid; j < array.Length; j++)
-            {
-                right[index++] = array[j];
-            }//for
+            for (int j = midArr; j < array.Length; j++)
+                rightArray[index++] = array[j];
 
-            MergeSort(left);
-            MergeSort(right);
-            Merge(left, right, array);
+            MergeSort(leftArray);
+            MergeSort(rightArray);
+            Merge(leftArray, rightArray, array);
         }//MergeSort
-    }
+    }//Partial Class - BasicSortingAlgorithms
 
-    public partial class BasicSortingAlgorithms /* Contains QuickSort */
+
+public partial class BasicSortingAlgorithms
     {
         /* https://en.wikipedia.org/wiki/Quicksort */
 
-        public static void QuickSort(int[] A, int start, int end)
+        public static void QuickSort(int[] array, int startIndex, int endIndex)
         {
-            if (end > start)
+            if (endIndex > startIndex)
             {
-                int pIndex = Partition(A, start, end);
-                QuickSort(A, start, pIndex - 1);
-                QuickSort(A, pIndex + 1, end);
+                int partitionIndex = Partition(array, startIndex, endIndex);
+                QuickSort(array, startIndex, partitionIndex - 1);
+                QuickSort(array, partitionIndex + 1, endIndex);
             }//if
         }//QuickSort
 
-        private static int Partition(int[] A, int start, int end) // moves elements smaller than pivot to left and elements larger to right
+        private static int Partition(int[] array, int startIndex, int endIndex) // moves elements smaller than pivot to leftArray and elements larger to rightArray
         {
-            int partitionIndex = start;
-            int pivot = A[end];
+            int partitionIndex = startIndex;
+            int pivot = array[endIndex];
             int temp = 0;
 
-            for (int i = start; i < end; i++)
+            for (int i = startIndex; i < endIndex; i++)
             {
-                if (A[i] <= pivot)
+                if (array[i] <= pivot)
                 {
-                    temp = A[i];
-                    A[i] = A[partitionIndex];
-                    A[partitionIndex] = temp;
+                    temp = array[i];
+                    array[i] = array[partitionIndex];
+                    array[partitionIndex] = temp;
                     partitionIndex++;
                 }//if
             }//for
-            temp = A[partitionIndex];
-            A[partitionIndex] = A[end];
-            A[end] = temp;
+
+            temp = array[partitionIndex];
+            array[partitionIndex] = array[endIndex];
+            array[endIndex] = temp;
 
             return partitionIndex;
         }//Partition
-    }
+    }//Partial Class - BasicSortingAlgorithms
 }
--- BasicAlgorithms/Test.cs
+++ BasicAlgorithms/Test.cs
@@ -4,7 +4,7 @@ namespace BasicAlgorithms
 {
     class Test
     {
-        private static readonly Random random = new Random();
+        private static readonly Random Random = new Random();
 
         static void Main(string[] args)
         {
@@ -15,10 +15,22 @@ namespace BasicAlgorithms
             Console.Write("Random Array: ");
             PrintArray(randomArray);
 
-            DisplaySortingAlgorithmSelectionConsole();
+            SortingAlgorithmConsole();
 
             int userSortingAlgorithmSelection = Convert.ToInt32(Console.ReadLine());
 
+            ExecuteSelectedAlgorithm(randomArray, userSortingAlgorithmSelection);
+
+            PrintArray(randomArray);
+
+            if (IsArraySorted(randomArray))
+                Console.WriteLine("\n\nArray has been successfully sorted!");
+            else
+                Console.WriteLine("\n\nArray was not successfully sorted...");
+        }//Main
+
+        private static void ExecuteSelectedAlgorithm(int[] randomArray, int userSortingAlgorithmSelection)
+        {
             switch (userSortingAlgorithmSelection)
             {
                 case 1:
@@ -52,20 +64,9 @@ namespace BasicAlgorithms
                         break;
                     }//case5 - QuickSort
             }//switch
+        }
 
-            PrintArray(randomArray);
-
-            if (IsArraySorted(randomArray))
-            {
-                Console.WriteLine("\n\nArray has been successfully sorted!");
-            }//if
-            else
-            {
-                Console.WriteLine("\n\nArray was not successfully sorted...");
-            }//else
-        }//Main
-
-        private static void DisplaySortingAlgorithmSelectionConsole()
+        private static void SortingAlgorithmConsole()
         {
             Console.WriteLine("\n\nAlgorithm Tester...\n");
             Console.WriteLine("1 --> BubbleSort");
@@ -74,14 +75,12 @@ namespace BasicAlgorithms
             Console.WriteLine("4 --> MergeSort");
             Console.WriteLine("5 --> QuickSort\n");
             Console.Write("Selection Basic Algorithm to Preform... ");
-        }//DisplaySortingAlgorithmSelectionConsole
+        }//SortingAlgorithmConsole
 
         private static void PrintArray(int[] randomArray)
         {
             foreach (var integer in randomArray)
-            {
                 Console.Write($"{integer} ");
-            }//foreach
         }//PrintArray
 
         private static int[] GenerateRandomArray(int arraySize)
@@ -89,9 +88,8 @@ namespace BasicAlgorithms
             var randomArray = new int[arraySize];
 
             for (int i = 0; i < arraySize; i++)
-            {
-                randomArray[i] = random.Next(100) + 1; /* array contains random numbers between 1 and 100 */
-            }//for
+                randomArray[i] = Random.Next(100) + 1; /* array contains random numbers between 1 and 100 */
+
             return randomArray;
         }//GenerateRandomArray
 
@@ -100,10 +98,116 @@ namespace BasicAlgorithms
             for (int i = 0; i < array.Length - 1; i++)
             {
                 if (array[i] > array[i + 1])
-                {
                     return false;
-                }//if
             }//for
+
+            return true;
+        }//IsArraySorted
+    }
+}
+using System;
+
+namespace BasicAlgorithms
+{
+    class Test
+    {
+        private static readonly Random Random = new Random();
+
+        static void Main(string[] args)
+        {
+            var randomArray = GenerateRandomArray(10);
+
+            Console.WriteLine("Random 10 Integer Array Generated...");
+
+            Console.Write("Random Array: ");
+            PrintArray(randomArray);
+
+            SortingAlgorithmConsole();
+
+            int userSortingAlgorithmSelection = Convert.ToInt32(Console.ReadLine());
+
+            ExecuteSelectedAlgorithm(randomArray, userSortingAlgorithmSelection);
+
+            PrintArray(randomArray);
+
+            if (IsArraySorted(randomArray))
+                Console.WriteLine("\n\nArray has been successfully sorted!");
+            else
+                Console.WriteLine("\n\nArray was not successfully sorted...");
+        }//Main
+
+        private static void ExecuteSelectedAlgorithm(int[] randomArray, int userSortingAlgorithmSelection)
+        {
+            switch (userSortingAlgorithmSelection)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Bubble Sort Algorithm Preformed...\n");
+                        BasicSortingAlgorithms.BubbleSort(randomArray);
+                        break;
+                    }//case1 - BubbleSort
+                case 2:
+                    {
+                        Console.WriteLine("Insertion Sort Algorithm Preformed...\n");
+                        BasicSortingAlgorithms.InsertionSort(randomArray);
+                        break;
+                    }//case2 - InsertionSort
+                case 3:
+                    {
+                        Console.WriteLine("Selection Sort Algorithm Preformed...\n");
+                        BasicSortingAlgorithms.SelectionSort(randomArray);
+                        break;
+                    }//case3 - SelectionSort
+                case 4:
+                    {
+                        Console.WriteLine("Merge Sort Algorithm Preformed...\n");
+                        BasicSortingAlgorithms.MergeSort(randomArray);
+                        break;
+                    }//case4 - MergeSort
+                case 5:
+                    {
+                        Console.WriteLine("Quick Sort Algorithm Preformed...\n");
+                        BasicSortingAlgorithms.QuickSort(randomArray, 0, randomArray.Length);
+                        break;
+                    }//case5 - QuickSort
+            }//switch
+        }
+
+        private static void SortingAlgorithmConsole()
+        {
+            Console.WriteLine("\n\nAlgorithm Tester...\n");
+            Console.WriteLine("1 --> BubbleSort");
+            Console.WriteLine("2 --> InsertionSort");
+            Console.WriteLine("3 --> SelectionSort");
+            Console.WriteLine("4 --> MergeSort");
+            Console.WriteLine("5 --> QuickSort\n");
+            Console.Write("Selection Basic Algorithm to Preform... ");
+        }//SortingAlgorithmConsole
+
+        private static void PrintArray(int[] randomArray)
+        {
+            foreach (var integer in randomArray)
+                Console.Write($"{integer} ");
+        }//PrintArray
+
+        private static int[] GenerateRandomArray(int arraySize)
+        {
+            var randomArray = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+                randomArray[i] = Random.Next(100) + 1; /* array contains random numbers between 1 and 100 */
+
+            return randomArray;
+        }//GenerateRandomArray
+
+        private static bool IsArraySorted(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                    return false;
+            }//for
+
             return true;
         }//IsArraySorted
     }
